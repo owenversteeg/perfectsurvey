770e2702f852d34b8dc5e03693ebd53e204c8bc3
Glenn Smith <couleeapps@gmail.com>
2016-05-03 10:47:33 -0400

    More Qs

--- survey.js
+++ survey.js
@@ -1,6 +1,26 @@
 var survey = {
 	"questions": [
 		{
+			"text": "What is your gender?",
+			"type": "option",
+			"options": {
+				"Male": "m",
+				"Female": "f",
+				"Other": "o"
+			}
+		},
+		{
+			"text": "What is your age?",
+			"type": "option",
+			"options": {
+				"14": "14",
+				"15": "15",
+				"16": "16",
+				"17": "17",
+				"18": "18"
+			}
+		},
+		{
 			"text": "How much time do you spend on the internet on an average weekday / weekend day?",
 			"type": "number"
 		},
@@ -14,9 +34,21 @@ var survey = {
 			}
 		},
 		{
+			"text": "Which of these sites do you have accounts on?",
+			"type": "option",
+			"options": {
+				"Facebook": "Facebook",
+				"Instagram": "instagram",
+				"Twitter": "twitter",
+				"tumblr": "tumblr",
+				"reddit": "reddit",
+				"YouTube": "youtube"
+			}
+		},
+		{
 			"text": "Have you ever done any programming?",
 			"type": "checkbox"
-		}
+		},
 	]
 };
 
