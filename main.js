407a9a56a2bbb488dfb1950c7b8040f1fdb13036
Owen Versteeg <me@owenversteeg.com>
2016-05-03 10:53:58 -0400

    fix ghostery detection

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var ghostery = false;
 var uBlock = false;
 
 if ($('#projectwonderful').width() === 1) ghostery = true;
-if ($('#projectwonderful').width() === 0) uBlock = true;
+if ($('#projectwonderful').width() === 0) {
+  uBlock = true;
+  ghostery = undefined;
+}
 
-alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock)
+alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ' undefined means we don\'t know')
