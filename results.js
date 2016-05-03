14981e1347819a783a1891cc5e603338146435aa
Glenn Smith <couleeapps@gmail.com>
2016-05-03 10:33:30 -0400

    Something

--- results.js
+++ results.js
@@ -1,12 +1,30 @@
-function blockingAds() {
+function isBlockingAdsByGoogle() {
 	return $('.adsbygoogle').css('display') === "none";
 }
-function blockingGA() {
+function isBlockingGA() {
 	return typeof(ga.da) === "undefined";
 }
 
+function isRunningGhostery() {
+	return isBlockingGA() && !isBlockingAdsByGoogle();
+}
+
+function isRunningSomeAdblock() {
+	return isBlockingAdsByGoogle();
+}
+
 setTimeout(function() {
-	document.getElementById("results").innerHTML = "Blocking: Ads: " + blockingAds() + " GA: " + blockingGA();
+	var running = "";
+	if (isRunningGhostery()) {
+		running += "Ghostery ";
+	}
+	if (isRunningSomeAdblock()) {
+		if (isBlockingGA()) {
+			running += "Possibly Ghostery ";
+		}
+		running += "Some Adblock ";
+	}
+	document.getElementById("results").innerHTML = "Running: " + running;
 }, 1000);
 
 document.getElementById("results").innerHTML = "Please standby for fake loading time";
