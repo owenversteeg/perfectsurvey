55c3a750d0155447041b6c1ff2112d1b384326dd
Owen Versteeg <me@owenversteeg.com>
2016-05-06 10:30:54 -0400

    fix name

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 setTimeout(function() {
 	var ghostery = false;
 	var uBlock = false;
-	var abp = false;
+	var getAdblock = false;
 
 	if ($('#projectwonderful').width() === 1) ghostery = true;
 	if ($('#projectwonderful').width() === 0) uBlock = true;
 	if ($('#websitealive').width() === 1) ghostery = true;
-	if ($('#doubleclick').css('opacity') === "0") abp = true;
+	if ($('#doubleclick').css('opacity') === "0") getAdblock = true;
 
-	alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ', using abp: ' + abp)
+	alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ', using adblock: ' + getAdblock)
 }, 1000);
