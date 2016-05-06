44629be69fa2ed0db81c1aafbd85a002420522db
Glenn Smith <couleeapps@gmail.com>
2016-05-06 10:13:42 -0400

    slow down

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var ghostery = false;
-var uBlock = false;
+setTimeout(function() {
+	var ghostery = false;
+	var uBlock = false;
 
-if ($('#projectwonderful').width() === 1) ghostery = true;
-if ($('#projectwonderful').width() === 0) {
-  uBlock = true;
-}
+	if ($('#projectwonderful').width() === 1) ghostery = true;
+	if ($('#projectwonderful').width() === 0) {
+		uBlock = true;
+	}
 
-if ($('#websitealive').width() === 1) ghostery = true;
+	if ($('#websitealive').width() === 1) ghostery = true;
 
-alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ' undefined means we don\'t know')
+	alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ' undefined means we don\'t know')
+}, 1000);
