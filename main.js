050a44c464aec0565c5a7cafb0da2d26bf78597e
Glenn Smith <couleeapps@gmail.com>
2016-05-09 10:15:42 -0400

    Debug

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ setTimeout(function() {
 	var uBlock = false;
 	var getAdblock = false;
 
+	var debug = $("#debug");
+
 	if ($('#projectwonderful').width() === 1) ghostery = true;
 	if ($('#projectwonderful').width() === 0) uBlock = true;
 	if ($('#websitealive').width() === 1) ghostery = true;
 	if ($('#doubleclick').css('opacity') === "0") getAdblock = true;
 
-	alert('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ', using adblock: ' + getAdblock + ', using ABP: ' + adblockPlus)
+	debug.append("#projectwonderful: " + $('#projectwonderful').width() + "<br>");
+	debug.append("#projectwonderful: " + $('#projectwonderful').width() + "<br>");
+	debug.append("#websitealive: " + $('#websitealive').width() + "<br>");
+	debug.append("#doubleclick: " + $('#doubleclick').css('opacity') + "<br>");
+
+
+
+	debug.append('using ghostery: ' + ghostery + ', using uBlock: ' + uBlock + ', using adblock: ' + getAdblock + ', using ABP: ' + adblockPlus)
 }, 1000);
